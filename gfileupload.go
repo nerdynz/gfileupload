c100a5f405f4fe41bf3b525e9f4db03b0a0e5f63
Joshua <josh@nerdy.co.nz>
2020-04-08 16:45:22 +1200

    bucket name fix

--- gfileupload.go
+++ gfileupload.go
@@ -3,6 +3,7 @@ package gfileupload
 import (
 	"bytes"
 	"context"
+	"errors"
 	"io"
 	"net/http"
 
@@ -13,18 +14,25 @@ func FromBytes(b []byte, objectName string, bucketName string, isPublic bool) (u
 	return FromFile(bytes.NewReader(b), objectName, bucketName, isPublic)
 }
 
-func FromRequest(req *http.Request, bucketName string, isPublic bool) (url string, err error) {
+func FromRequest(req *http.Request, bucketName string, isPublic bool) (filename string, url string, err error) {
 	req.ParseMultipartForm(32)
 	file, handler, err := req.FormFile("file")
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	filename := handler.Filename
 	defer file.Close()
-	return FromFile(file, filename, bucketName, isPublic)
+	filename = handler.Filename
+	url, err = FromFile(file, filename, bucketName, isPublic)
+	if err != nil {
+		return "", "", err
+	}
+	return filename, url, err
 }
 
 func FromFile(file io.Reader, objectName string, bucketName string, isPublic bool) (url string, err error) {
+	if bucketName == "" {
+		return "", errors.New("bucketName not specified")
+	}
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx) // guessing this is grpc.. TODO REFACTOR. this should only be initialzed once
 	if err != nil {
@@ -55,11 +63,10 @@ func FromFile(file io.Reader, objectName string, bucketName string, isPublic boo
 			return "", err
 		}
 	}
-	attrs, err := obj.Attrs(ctx)
-	if err := w.Close(); err != nil {
-		return "", err
-	}
-
-	url = attrs.MediaLink
+	// attrs, err := obj.Attrs(ctx)
+	// if err := w.Close(); err != nil {
+	// 	return "", err
+	// }
+	url = "https://storage.googleapis.com/" + bucketName + "/" + objectName
 	return url, nil
 }
